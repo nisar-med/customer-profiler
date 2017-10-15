018293f0eb54fd7150ca6f51b2bbebd0bd4da8bc
Nisar Ahmed <nisar.med@gmail.com>
2017-10-15 16:08:31 +1100

    add function to return transactions by customer

--- src/main/java/com/ing/customerprofiler/data/repository/TransactionRepository.java
+++ src/main/java/com/ing/customerprofiler/data/repository/TransactionRepository.java
@@ -4,6 +4,9 @@ import com.ing.customerprofiler.data.entity.Transaction;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends CrudRepository<Transaction, Long> {
+    public List<Transaction> findByCustomerId(Long customerId);
 }
