ab3376e98e93a6ad925c2a73e7d840dc85906b60
Nisar Ahmed <nisar.med@gmail.com>
2017-10-15 14:30:53 +1100

    add entity and id annotations and getters/setters

--- src/main/java/com/ing/customerprofiler/data/entity/Transaction.java
+++ src/main/java/com/ing/customerprofiler/data/entity/Transaction.java
@@ -1,10 +1,46 @@
 package com.ing.customerprofiler.data.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.Date;
 
+@Entity
 public class Transaction {
+    @Id
     private long customerId;
     private Date date;
     private double amount;
     private String description;
+
+    public long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
