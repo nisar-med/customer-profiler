0e313dd11b9c526ebc9849a189757f348e78f819
Nisar Ahmed <nisar.med@gmail.com>
2017-10-15 15:05:25 +1100

    add missing Repository annotation so that it can be scanned

--- src/main/java/com/ing/customerprofiler/data/repository/TransactionRepository.java
+++ src/main/java/com/ing/customerprofiler/data/repository/TransactionRepository.java
@@ -2,6 +2,8 @@ package com.ing.customerprofiler.data.repository;
 
 import com.ing.customerprofiler.data.entity.Transaction;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransactionRepository extends CrudRepository<Transaction, Long> {
 }
