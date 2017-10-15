3e3e3a2217588cc5712b5081fef868ef3ffd918d
Nisar Ahmed <nisar.med@gmail.com>
2017-10-15 16:08:41 +1100

    add unit test

--- src/test/java/com/ing/customerprofiler/CustomerProfilerApplicationTests.java
+++ src/test/java/com/ing/customerprofiler/CustomerProfilerApplicationTests.java
@@ -1,7 +1,10 @@
 package com.ing.customerprofiler;
 
+import com.ing.customerprofiler.data.service.TransactionService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CustomerProfilerApplicationTests {
 
+	@Autowired
+	TransactionService transactionService;
+
 	@Test
 	public void contextLoads() {
+	}
+	@Test
+	public void testTransactionCount() {
+		Assert.assertTrue(transactionService.getTransactions().stream().count() == 2995);
 	}
 
 }
