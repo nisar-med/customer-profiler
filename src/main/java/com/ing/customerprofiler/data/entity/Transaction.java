eff6cd6408269fcff6d10d8b478609fd0bc1e9bf
Nisar Ahmed <nisar.med@gmail.com>
2017-10-15 14:03:18 +1100

    add data  - create Transaction Entity  - add data schema and data import file  - setup h2 and jpa properties

---
+++ src/main/java/com/ing/customerprofiler/data/entity/Transaction.java
@@ -0,0 +1,10 @@
+package com.ing.customerprofiler.data.entity;
+
+import java.util.Date;
+
+public class Transaction {
+    private long customerId;
+    private Date date;
+    private double amount;
+    private String description;
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,1 @@
+CREATE TABLE Transaction (CustomerId INT, Date TIMESTAMP, Amount DOUBLE, Description VARCHAR(128));
